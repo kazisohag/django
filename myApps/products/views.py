9145dfc8b7bf006e241badbfbaf1e718fa429a5c
kazisohag <sohag15-543@diu.edu.bd>
2020-03-16 01:03:20 +0600

    Solve Views Problems..

--- myApps/products/views.py
+++ myApps/products/views.py
@@ -1,10 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Products
 # Create your views here.
 
 
+
 def product_view(request):
-    obj = Products.objects.get('id = 1')
+
+    obj = Products.objects.get(id = 1)
+    
     context = {
         'name': obj.title,
         'descrip': obj.descripssion,
